d5fe60378cb54dddbecb559e02a57b0045fcc3bc
Rohith Rachamalla <57551454+RohithRachamalla12@users.noreply.github.com>
2022-03-14 05:16:18 -0500

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,8 @@ def newMaven()
 {
    sh 'mvn package'
 }
+
+def newDeploy(ipaddress, conextpath)
+{
+  scp /var/lib/jenkins/workspace/DeclarativePipeline2/webapp/target/webapp.war ubuntu@"${ipaddress}":/var/lib/tomcat9/webapp/"${contextpath}.war"
+}
